43b7eb6ae3277bd5203a374253f31360642f0ca9
conanstar <conanstar@gmail.com>
2014-11-25 14:51:28 +0800

    test 2
    test test

--- app/src/main/java/com/studio/conan/gin/MyActivity.java
+++ app/src/main/java/com/studio/conan/gin/MyActivity.java
@@ -12,9 +12,6 @@ public class MyActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
-
-
-
     }
 
 
